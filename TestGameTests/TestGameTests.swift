1a228f245934ec44b59989362bea682c7b0a43c3
Solomiya Pylypiv <sololita@umich.edu>
2025-01-27 09:14:58 -0500

    Initial Commit

---
+++ TestGameTests/TestGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestGameTests.swift
+//  TestGameTests
+//
+//  Created by Solomiya Pylypiv on 1/27/25.
+//
+
+import Testing
+@testable import TestGame
+
+struct TestGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
